64c2ecd81a903b5fdb649e153c13c70ff0c3110b
Hervé Millet <hmillet@gmail.com>
2018-01-25 16:10:33 +0100

    chat - basics ok

--- index.js
+++ index.js
@@ -59,29 +59,47 @@ wss.on("connection", function connection(wsclient, req) {
         const O_message = JSON.parse(message);
         if (O_message && O_message.username) {
           if (connectedUser.hasOwnProperty(O_message.username)) {
-            wsclient.send("ERROR : username already in use");
-          } else {
-            currentUser.username =  O_message.username;
+            //wsclient.send("ERROR : username already in use");
+            currentUser.username = O_message.username;
             currentUser.authenticated = true;
             connectedUser[O_message.username] = currentUser;
-            console.log("o-- new User connected : "+currentUser.username);
-            console.log(">-- Number of users connected : "+Object.keys(connectedUser).length);
+            console.log("o-- User reconnected : " + currentUser.username);
+            console.log(">-- Number of users connected : " + Object.keys(connectedUser).length);
           }
-        } else {
-          wsclient.send("ERROR : you are not authenticated");
-          console.log("<-- authentication failed : "+O_message);
+          else {
+            currentUser.username = O_message.username;
+            currentUser.authenticated = true;
+            connectedUser[O_message.username] = currentUser;
+            console.log("o-- new User connected : " + currentUser.username);
+            console.log(">-- Number of users connected : " + Object.keys(connectedUser).length);
+          }
         }
-      } catch (e) {
-        console.log("<-- authentication failed : "+message);
+        else {
+          wsclient.send("ERROR : you are not authenticated");
+          console.log("<-- authentication failed : " + O_message);
+        }
       }
-    } else {
-      console.log(">-- message from : '"+currentUser.username+"' : "+message);
+      catch (e) {
+        wsclient.send("ERROR : you are not authenticated");
+        console.log("<-- authentication failed : " + message);
+      }
+    }
+    else {
+      console.log(">-- message from : '" + currentUser.username + "' : " + message);
+      for (var username in connectedUser) {
+        if (connectedUser.hasOwnProperty(username)) {
+          let user = connectedUser[username];
+          if (user.username !== currentUser.username && user.client.readyState === WebSocket.OPEN) {
+            user.client.send(message);
+          }
+        }
+      }
     }
   });
 
   wsclient.on("close", function onclose() {
-    console.log("x-- User disconnected : "+currentUser.username);
-    console.log(">-- Number of users connected : "+(connectedUser.length > 0 ? connectedUser.length : 0));
+    console.log("x-- User disconnected : " + currentUser.username);
+    console.log(">-- Number of users connected : " + (connectedUser.length > 0 ? connectedUser.length : 0));
     delete currentUser.username;
   });
 });
